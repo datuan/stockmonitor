5d3acbfeae9eed4426938d600f5483d6ab396d50
Tuan Dao <mrdaoanhtuan@gmail.com>
2016-08-22 15:44:14 -0700

    GoogleFinance

--- StockWebApp/src/main/java/com/example/restapi/StockApi.java
+++ StockWebApp/src/main/java/com/example/restapi/StockApi.java
@@ -1,8 +1,7 @@
 package com.example.restapi;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import java.util.List;
+
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import com.example.stockmonitor.data.Stock;
 import com.example.stockmonitor.dataaccess.DataAccess;
 import com.example.stockmonitor.dataaccess.DataAccessException;
@@ -10,43 +9,49 @@ import com.example.stockmonitor.dataaccess.SQLDataAccess;
 import com.example.stockmonitor.dataaccess.util.SQLDataPool;
 import com.example.stockmonitor.dataaccess.util.TomcatMySQLDataPool;
 
-@Path("/company")
-public class Company {
-	SQLDataPool pool=new TomcatMySQLDataPool();
-	String symbol;
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String getCompany(@QueryParam("symbol") String symbol){
-//		if (symbol==null){//symbol.length()==0){
-//			return "All companies";
-//		}
-//		return "Query company = "+symbol;
-//	}
+@Path("/stock")
+public class StockApi {
+	private static SQLDataPool pool=new TomcatMySQLDataPool();
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getAllCompanies(){
+	@Produces("application/json")
+	public List<Stock> getAllCompanies(){
+		DataAccess da=new SQLDataAccess(pool);
 		try{
-			InitialContext ic=new InitialContext();
-			DataSource ds=(DataSource)ic.lookup("java:comp/env/jdbc/StockDB");
-			
+			List<Stock> stocks=da.listCompany("user1");
+			return stocks;
 		}
-		catch(Exception e){
+		catch(DataAccessException e){
 			e.printStackTrace();
+			return null;
 		}
-		return "All companies";
 	}
 	
 	@GET @Path("/{symbol}")
 	@Produces("application/json")
-	public Stock getCompany(@PathParam("symbol") String symbol){
-		Stock stock=new Stock("abc", symbol, 10.0, 11.0, 15.2, "now",14325435);
-		//public Stock(String name, String symbol, double lastTradePrice, double ask, double bid, String lastTradeTime, long queryTime){
-		return stock;
+	public List<Stock> getCompany(@PathParam("symbol") String symbol){
+		DataAccess da=new SQLDataAccess(pool);
+		try{
+			List<Stock> stocks=da.companyHistory(symbol);
+			return stocks;
+		}
+		catch(DataAccessException e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	@DELETE @Path("/{symbol}")
 	public String deleteCompary(@PathParam("symbol") String symbol){
-		return "Delete symbol: "+symbol;
+		DataAccess da=new SQLDataAccess(pool);
+		try{
+			da.deleteCompany("user1", symbol);
+			return "Symbol: "+symbol+" deleted";
+		}
+		catch(DataAccessException e){
+			e.printStackTrace();
+			return "Error, msg="+e.getMessage();
+		}
 	}
 	
 	@POST @Path("/{symbol}")
@@ -54,7 +59,7 @@ public class Company {
 		//TODO: add a new company (symbol) to MySQL
 		DataAccess da=new SQLDataAccess(pool);
 		try{
-			da.addCompany("user1", symbol);
+			da.addSymbol("user1", symbol);
 			return "Symbol: "+symbol+" added";
 		}
 		catch(DataAccessException e){
--- StockWebApp/src/main/java/com/example/stockmonitor/dataaccess/util/TomcatMySQLDataPool.java
+++ StockWebApp/src/main/java/com/example/stockmonitor/dataaccess/util/TomcatMySQLDataPool.java
@@ -1,8 +1,5 @@
 package com.example.stockmonitor.dataaccess.util;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 
@@ -10,10 +7,15 @@ public class TomcatMySQLDataPool implements SQLDataPool {
 	private static DataSource ds=null;
 	private static void initDataSource(){
 		PoolProperties p=new PoolProperties();
-		p.setUrl("jdbc:mysql://10.0.1.14:3306/stock?useSSL=false");
+		p.setUrl("jdbc:mysql://localhost:3306/stock?useSSL=false");
 		p.setDriverClassName("com.mysql.jdbc.Driver");
-		p.setUsername("tuan");
-		p.setPassword("password");
+		p.setUsername("root");
+		p.setPassword("tuandao");
+		p.setJmxEnabled( true);
+	    p.setTestWhileIdle( true);
+	    p.setTestOnBorrow( true);
+	    p.setValidationQuery( "SELECT 1");
+	    p.setTestOnReturn( false);
 		p.setMaxActive(100);
         p.setInitialSize(5);
         p.setMaxWait(10000);
@@ -23,8 +25,9 @@ public class TomcatMySQLDataPool implements SQLDataPool {
         p.setLogAbandoned(true);
         p.setRemoveAbandoned(true);
         p.setJdbcInterceptors(
-          "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
-          "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+                "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"
+                + "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer;"
+                + "org.apache.tomcat.jdbc.pool.interceptor.ResetAbandonedTimer");
         ds = new DataSource();
         ds.setPoolProperties(p);
 	}
