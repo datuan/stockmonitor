5bb8bb5ee413ebd46a920db64188a7f0530d3567
Tuan Dao <mrdaoanhtuan@gmail.com>
2016-08-23 20:27:56 -0700

    update build script

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ dbhost=localhost:3306
 #database name, default=stock
 dbname=stock
 #username to access db
-dbuser=tuandao2
+dbuser=root
 #password
-dbpass=password2
+dbpass=tuandao
 #path to tomcat webapp folder
-tomcatPath=/Users/tuandao/tomcat/apache-tomcat-8.0.36/webapps/
+tomcatPath=/Users/tuandao/tomcat/apache-tomcat-8.0.36/webapps
 
 #define variables
 webconfig=StockWebApp/src/main/webapp/WEB-INF/web.xml
 
-if [ ! -f $ $webconfig ]; then
+if [ ! -f $webconfig ]; then
 	echo "Cannot find StockWebApp/src/main/webapp/WEB-INF/web.xml, please check path name"
 	exit 1
 fi
@@ -28,7 +28,7 @@ sed -i -e "s,<\!-- dbuser -->.*,<\!-- dbuser --><env-entry-value>"$dbuser"<\/env
 sed -i -e "s,<\!-- dbpassword -->.*,<\!-- dbpassword --><env-entry-value>"$dbpass"<\/env-entry-value>,g"  $webconfig
 
 dconf=daemon.conf
-if [ ! -f $ $dconf ]; then
+if [ ! -f $dconf ]; then
 	echo "Cannot find daemon.conf, please check path name"
 	exit 1
 fi
@@ -38,17 +38,49 @@ sed -i -e "s,dbname=.*,dbhost="$dbname",g" $dconf
 sed -i -e "s,dbuser=.*,dbhost="$dbuser",g" $dconf
 sed -i -e "s,dbpassword=.*,dbpassword="$dbpass",g" $dconf
 
+
+echo "*********************************"
+echo "*********************************"
 echo "Build Daemon Process..."
+echo "*********************************"
+echo "*********************************"
+
+
 mvn install -Dmaven.test.skip=true -f StockDaemon/pom.xml
 if [ $? -ne 0 ];then
 	echo "Building daemon fails, exit"
 	exit 1
 fi
+
+
+echo "*********************************"
+echo "*********************************"
 echo "Build WebApp ..."
+echo "*********************************"
+echo "*********************************"
+
+
 mvn package -f StockWebApp/pom.xml
 if [ $? -ne 0 ];then
 	echo "Building web app fails, exit"
 	exit 1
 fi
+
+echo "*********************************"
+echo "*********************************"
 echo "Copy web app to tomcat folder"
-cp StockWebApp/target/StockWebApp.war $tomcatPath 
+echo "*********************************"
+echo "*********************************"
+
+
+if [ ! -d $tomcatPath ];then
+	echo "Cannot find tomcat folder, please manually deploy webapp"
+	exit 1
+fi
+cp StockWebApp/target/StockWebApp.war $tomcatPath
+
+echo "Sleep for 20 seconds, waiting for web app to be deployed"
+sleep 20
+echo "Running test ... "
+mvn test -f StockDaemon/pom.xml
+echo "Please open browser to test the GUI interface"
